68964b5dc10e330fadcf8cd5b57583c6a543b03e
NeiverAlvarez <17917355-NeiverAlvarez@users.noreply.replit.com>
2025-02-28 00:13:38 +0000

    Remove social network link from header; rename remaining social link to "Social Network".
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/ecb7959a-10a2-43c2-b3de-f9c2a2fb7282/c1426574-5ae8-433f-ae34-73f954a25409.jpg

--- client/src/components/layout/header.tsx
+++ client/src/components/layout/header.tsx
@@ -129,23 +129,12 @@ export function Header() {
                 </Button>
               </Link>
               
-              {/* World Icon - With Navigation to Social Network */}
-              <Link href="/social-network">
-                <Button size="sm" variant="ghost" className="text-white hover:bg-[#2A2A2A] p-2">
-                  <img 
-                    src="/assets/world-icon.svg" 
-                    alt="World Icon" 
-                    className="h-4 w-4" 
-                  />
-                </Button>
-              </Link>
-              
-              {/* Firestore Social Network Icon */}
+              {/* Social Network Icon (Firestore) */}
               <Link href="/firestore-social">
                 <Button size="sm" variant="ghost" className="text-white hover:bg-[#2A2A2A] p-2 mr-2">
                   <img 
                     src="/assets/world-icon.svg" 
-                    alt="Firestore Social" 
+                    alt="Social Network" 
                     className="h-4 w-4 filter hue-rotate-60" 
                   />
                 </Button>
